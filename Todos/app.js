e1ba0b7dbd20a8f732c45ff61c8835785549bd7e
aurevia-1 <info.aureviacreations@gmail.com>
2025-10-31 14:40:49 +0500

    New Links Edit

--- Todos/app.js
+++ Todos/app.js
@@ -252,7 +252,7 @@ let deletetodo = (key) =>{
 window.logout = ()=>{
   signOut(auth)
   .then(()=>{
-    window.location.href = "index.html"
+    window.location.replace = ("../index.html")
   })
   .catch(()=>{
     console.log("Not logout")
